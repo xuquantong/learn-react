eb42458755e3d1ba5b88f4d0231ea6817491ca05
xuquantong <xqt_123@163.com>
2021-07-31 14:16:08 +0800

    react 模拟vue slot类似的功能

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ In the project directory, you can run:
 
 ### `yarn start`
 
-
-
+模拟Vue中的slot 虽然React中没有Vue的插槽，但可以实现类似功能，根本原理就是jsx可以当成参数传递给组件；  
+- 可以将jsx放在元素内部，子组件可通过`props.children`获取  
+- 也可以将jsx放在属性中传递给子组件，子组件通过`props`获取
+  
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import ChildComponent from './ChildComponent'
 
 export default class App extends Component {
   render () {
     return (
       <div>
         <h2>React Demo</h2>
-        <p>branch tabcontrol：父子组件传值demo</p>
+        {/* <ChildComponent>
+          <div>first div<p>first div p</p></div>
+          <div>second div</div>
+          <div>another div</div>
+        </ChildComponent> */}
+        <ChildComponent
+          first={<div>first div<p>first div p</p></div>}
+          second={<div>second div</div>}
+          another={<div>another div</div>}
+        />
       </div>
     )
   }
