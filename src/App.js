55996ef1af1430e6a19af3b17286c1f1716947c7
xuquantong <xqt_123@163.com>
2021-07-31 13:02:39 +0800

    初始化master

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react'
-import TabControl from './TabControl'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentTitle: '最新',
-      titles: ['最新', '精选', '流行']
-    }
-  }
   render () {
-    const { titles, currentTitle } = this.state
     return (
       <div>
-        <TabControl
-          titles={titles}
-          itemClick={index => this.itemClick(index)}
-        />
-        <h2>{currentTitle}</h2>
+        <h2>React Demo</h2>
+        <p>branch tabcontrol：父子组件传值demo</p>
       </div>
     )
-  }
-  itemClick (index) {
-    this.setState({
-      currentTitle: this.state.titles[index]
-    })
   }
 }
