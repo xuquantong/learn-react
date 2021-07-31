32d5358bfcea9daf361a79104c120bc7079c49da
xuquantong <xqt_123@163.com>
2021-08-01 00:38:18 +0800

    demo-slot

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react'
-import ChildComponent from './ChildComponent'
+import React, { Component, createRef } from 'react'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.titleRef = createRef()
+    this.titleP = null
+  }
   render () {
     return (
       <div>
         <h2>React Demo</h2>
-        {/* <ChildComponent>
-          <div>first div<p>first div p</p></div>
-          <div>second div</div>
-          <div>another div</div>
-        </ChildComponent> */}
-        <ChildComponent
-          first={<div>first div<p>first div p</p></div>}
-          second={<div>second div</div>}
-          another={<div>another div</div>}
-        />
+        <p ref="thisp">this is p</p>
+        <p ref={this.titleRef}>this is another p</p>
+        <p ref={arg => this.titleP = arg}>this is third p</p>
+        <button onClick={e => this.changeIt()}>click me</button>
       </div>
     )
+  }
+  changeIt () {
+    console.log(this.refs.thisp)
+    this.refs.thisp.innerHTML = 'xxxxx'
+
+    console.log(this.titleRef.current)
+    this.titleRef.current.innerHTML = 'xxxxx'
+
+    console.log(this.titleP)
+    this.titleP.innerHTML = 'xxxxx'
   }
 }
