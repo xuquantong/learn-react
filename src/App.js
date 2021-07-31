4b128fe2cca9c3a4e370943d544bdeb936c8ef57
xuquantong <xqt_123@163.com>
2021-07-31 12:45:22 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TabControl from './TabControl'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentTitle: '最新',
+      titles: ['最新', '精选', '流行']
+    }
+  }
+  render () {
+    const { titles, currentTitle } = this.state
+    return (
+      <div>
+        <TabControl
+          titles={titles}
+          itemClick={index => this.itemClick(index)}
+        />
+        <h2>{currentTitle}</h2>
+      </div>
+    )
+  }
+  itemClick (index) {
+    this.setState({
+      currentTitle: this.state.titles[index]
+    })
+  }
 }
-
-export default App;
